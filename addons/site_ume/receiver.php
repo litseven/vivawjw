170049bced4dd4a13c367845f750b0cd3b79b8ea
litseven <litseven27@sina.com>
2017-01-05 10:39:27 +0800

    测试

--- addons/site_ume/receiver.php
+++ addons/site_ume/receiver.php
@@ -11,7 +11,7 @@ class PicTranModuleReceiver extends WeModuleReceiver {
     public function receive() {
         $type = $this->message['type'];
         //这里定义此模块进行消息订阅时的, 消息到达以后的具体处理过程, 请查看微擎文档来编写你的代码
-
+//测试
 
     }
 }
