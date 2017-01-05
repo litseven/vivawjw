52202cb7fa1d04e24c805a3e924e0236e017d5aa
litseven <litseven27@sina.com>
2017-01-05 10:49:05 +0800

    1.0

--- addons/site_ume/receiver.php
+++ addons/site_ume/receiver.php
@@ -9,8 +9,7 @@ defined('IN_IA') or exit('Access Denied');
 
 class PicTranModuleReceiver extends WeModuleReceiver {
     public function receive() {
-
-        
+        //123
         $type = $this->message['type'];
         //这里定义此模块进行消息订阅时的, 消息到达以后的具体处理过程, 请查看微擎文档来编写你的代码
     }
