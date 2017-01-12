b3c0d1655d98567b6e57cfb7dbce10b6890a05bf
litseven <litseven27@sina.com>
2017-01-12 12:20:22 +0800

    1.0

--- addons/vivanjyh_xytp/site.php
+++ addons/vivanjyh_xytp/site.php
@@ -12,7 +12,6 @@ class Vivanjyh_xytpModuleSite extends WeModuleSite {
     //首页
     public function doMobileVote(){
         global $_W,$_GPC;
-        $op =trim($_GPC['op'])? trim($_GPC['op']): 'jingli';
         if (empty($_W['fans']['nickname'])) {
             mc_oauth_userinfo();
         }
@@ -47,6 +46,13 @@ class Vivanjyh_xytpModuleSite extends WeModuleSite {
         $day = date("d");
         $start = mktime(0, 0, 0, $month, $day, $year);
         $end = mktime(23, 59, 59, $month, $day, $year);
+
+         //判断是2017/1/12中午12点结束
+        $end4 = strtotime(date("Y-m-d H:i:s",mktime(12,00,00,1,12,2017)));
+        $time = time();
+        if($time > $end4){
+            echo 300;exit;
+        }
         //查找是有有投票信息
         $selde = pdo_fetchall('SELECT degree FROM '.tablename('vivanjyh_xytp_num').' WHERE uid = :uid AND sittime>=:start AND sittime<=:end ORDER BY  degree ASC LIMIT 1',array(':uid'=>$uid, ':start' => $start, ':end' => $end));
         //如果有次数degree-1
@@ -81,6 +87,91 @@ class Vivanjyh_xytpModuleSite extends WeModuleSite {
             echo $degree;exit;
         }
     }
+
+
+
+
+
+
+
+    //周五活动
+    public function doMobileSec(){
+        global $_W;$_GET;
+        if (empty($_W['fans']['nickname'])) {
+            mc_oauth_userinfo();
+        }
+        include $this->template('sec');
+    }
+    public function doMobileSeclist(){
+        global $_W;$_GET;
+        $datalist = pdo_fetchall('SELECT * FROM '.tablename('vivanjyh_xytp_sec').' WHERE uniacid=:uniacid ORDER BY votes desc,id asc',array(':uniacid'=>$_W['uniacid']));
+        include $this->template('seclist');
+    }
+    //周五活动投票
+    public function doMobileVotepostsec(){
+        global $_W,$_GPC;
+        /*---------------------------------------------------------------------------------------------------------------------------------*/
+        $uid =  $_W['member']['uid'];
+        //$uid = 8;
+        if(!$uid){
+            echo 500;exit;
+        }
+        $check_val = $_GPC['check_val'];
+        $year = date("Y");
+        $month = date("m");
+        $day = date("d");
+        $start = mktime(0, 0, 0, $month, $day, $year);
+        $end = mktime(23, 59, 59, $month, $day, $year);
+
+        $start4 = strtotime(date("Y-m-d H:i:s",mktime(9,30,00,1,13,2017)));
+        $end4 = strtotime(date("Y-m-d H:i:s",mktime(11,29,59,1,13,2017)));
+        $time = time();
+        if($time < $start4){
+            echo 100;exit;
+        }
+        if($time > $end4){
+            echo 300;exit;
+        }
+        //查找是有有投票信息
+        $selde = pdo_fetchall('SELECT degree FROM '.tablename('vivanjyh_xytp_secnum').' WHERE uid = :uid AND sittime>=:start AND sittime<=:end ORDER BY  degree ASC LIMIT 1',array(':uid'=>$uid, ':start' => $start, ':end' => $end));
+        //如果有次数degree-1
+        if ($selde) {
+            $degree = $selde[0]['degree'] - 1;
+        } else {
+            //如果没有初始值为2
+            $degree = 0;
+        }
+        //次数用完（0-1）
+        if ($degree == -1){
+            echo -1;exit;
+        }
+        //插入数据
+        foreach ($check_val as $val){
+            $indata = array(
+                'uniacid' => $_W['uniacid'],
+                'uid' => $uid,
+                'numid' => $val,
+                'degree' => $degree,
+                'sittime' => time()
+            );
+            $votes = pdo_fetch('SELECT votes FROM '.tablename('vivanjyh_xytp_sec').' WHERE numid = :numid',array(':numid'=>$val));
+            if($votes['votes']){
+                pdo_update('vivanjyh_xytp_sec',array('votes'=>$votes['votes']+1),array('numid'=>$val));
+            }else{
+                pdo_update('vivanjyh_xytp_sec',array('votes'=>1),array('numid'=>$val));
+            }
+            $data = pdo_insert('vivanjyh_xytp_secnum', $indata);
+        }
+        if ($data) {
+            echo $degree;exit;
+        }
+    }
+
+
+
+
+
+
     //后台
 	public function doWebAdmin() {
 		//这个操作被定义用来呈现 规则列表
@@ -89,11 +180,8 @@ class Vivanjyh_xytpModuleSite extends WeModuleSite {
         $columns = intval($_GPC['columns']);
         $pindex =max(1, intval($_GPC['page']));
         $psize =10;
-        $total = pdo_fetchcolumn('SELECT COUNT(*) FROM ' . tablename('vivanjyh_xytp'). ' WHERE uniacid = :uniacid AND columns = :columns',array(':uniacid'=>$_W['uniacid'],':columns'=>$columns));
-        $list = pdo_fetchall('SELECT * FROM '.tablename('vivanjyh_xytp').' WHERE uniacid = :uniacid AND columns = :columns LIMIT '.($pindex - 1) * $psize.','.$psize,array(':uniacid'=>$_W['uniacid'],':columns'=>$columns));
-        foreach ($list as &$value){
-            $value['vote'] = pdo_fetchcolumn('SELECT COUNT(*) FROM '.tablename('vivanjyh_xytp_num').' WHERE  numid = :numid',array(':numid'=>$value['numid']));
-        }
+        $total = pdo_fetchcolumn('SELECT COUNT(*) FROM ' . tablename('vivanjyh_xytp'). ' WHERE uniacid = :uniacid ',array(':uniacid'=>$_W['uniacid']));
+        $list = pdo_fetchall('SELECT * FROM '.tablename('vivanjyh_xytp').' WHERE uniacid = :uniacid ORDER BY votes desc,numid asc LIMIT '.($pindex - 1) * $psize.','.$psize,array(':uniacid'=>$_W['uniacid']));
         $pager =pagination($total, $pindex, $psize);
         include $this->template('admin');
 	}
@@ -105,7 +193,7 @@ class Vivanjyh_xytpModuleSite extends WeModuleSite {
     }
     //清除票数
     public function doMobileDelvotes(){
-        pdo_update('vivanjyh_xytp',array('votes'=> ''));
+        pdo_update('vivanjyh_xytp',array('votes'=> 0));
     }
 /*    //添加数据
     public function doMobileAdd(){
---
+++ addons/vivanjyh_xytp/template/resource/js5/index.js
@@ -0,0 +1,95 @@
+//规则按钮初始位置
+//window.onload = function(){	
+//	setTimeout(function(){
+//		if(document.getElementsByClassName('ruleImg')[0]){
+//			document.getElementsByClassName('ruleImg')[0].style.left='70px';
+//		}		
+//	},2000);	
+//}
+//跳转至内页
+function toListPage(){
+	location.href='list.html';
+}
+function showRule(){
+	document.getElementById('rulemaskId').style='block';
+}
+function closeMask(){
+	document.getElementById('rulemaskId').style='none';
+}
+//投票
+function onChoice(obj){
+	var currImg = obj.getElementsByTagName('img')[0].src;
+	var sum= parseInt(obj.getElementsByTagName('span')[0].innerText);	
+	if(currImg.indexOf('choice2-icon.png') > 0){ 
+	    // 选择
+		obj.getElementsByTagName('img')[0].src='img/choice-icon.png';
+		obj.getElementsByTagName('span')[0].innerText = sum + 1;
+		//obj.getElementsByClassName('checkedFlag')[0].value = 1;
+		//obj.getElementsByClassName('num')[0].value = sum + 1;
+	}else{
+		// 取消选择
+		obj.getElementsByTagName('img')[0].src='img/choice2-icon.png';
+		obj.getElementsByTagName('span')[0].innerText = sum - 1;
+		//obj.getElementsByClassName('checkedFlag')[0].value = 0;
+		//obj.getElementsByClassName('num')[0].value = sum - 1;
+	}
+	
+	
+}
+function onMuc(){
+	var a=document.getElementById('music');
+	if(a.paused){
+		a.play();
+	}else{
+		a.pause();
+	}
+}
+//底部导航切换
+//function clearNavStyle(){
+//	var nav1 = document.getElementById('nav1');
+//	nav1.getElementsByTagName('img')[0].src= 'img/bottonbtn1.png';
+//	nav1.getElementsByTagName('div')[0].style.color='#d0a306';
+//	
+//	var nav2 = document.getElementById('nav2');
+//	nav2.getElementsByTagName('img')[0].src= 'img/bottonbtn2.png';
+//	nav2.getElementsByTagName('div')[0].style.color='#d0a306';
+//	
+//	var nav3 = document.getElementById('nav3');
+//	nav3.getElementsByTagName('img')[0].src= 'img/bottonbtn3.png';
+//	nav3.getElementsByTagName('div')[0].style.color='#d0a306';
+//}
+
+//客户经理
+function toItem1(){
+	clearNavStyle();
+	var nav1 = document.getElementById('nav1');
+	nav1.getElementsByTagName('img')[0].src= 'img/bottonbtn1-hover.png';
+	nav1.getElementsByTagName('div')[0].style.color='#6f4710';
+}
+//中行及部门
+function toItem2(){
+	clearNavStyle();
+	var nav2 = document.getElementById('nav2');
+	nav2.getElementsByTagName('img')[0].src= 'img/bottonbtn2-hover.png';
+	nav2.getElementsByTagName('div')[0].style.color='#6f4710';
+	
+}
+//中支及直属支行
+function toItem3(){
+	clearNavStyle();
+	var nav3 = document.getElementById('nav3');
+	nav3.getElementsByTagName('img')[0].src= 'img/bottonbtn3-hover.png';
+	nav3.getElementsByTagName('div')[0].style.color='#6f4710';
+}
+
+//$(function () {
+//	$(window).on('resize', function () {
+//		if ($('body').height() < 400) {
+//			alert(1111)
+//			$('.nav_box').css('display', 'none');
+//		}else{
+//			$('.nav_box').css('display', 'block');
+//		}
+//	})
+//})
+
